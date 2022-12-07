d22c1bb58a2b91b1d816d6cf387e829f30fb1af2
swaaalloww <sakaroki@gmail.com>
2022-12-07 10:18:55 +0800

    LineBot add

---
+++ LineBot/emoji.py
@@ -0,0 +1,18 @@
+
+# Text message example with LINE emoji
+{
+    "type": "text",
+    "text": "$ LINE emoji $", #開頭結尾有emoji
+    "emojis": [
+      {
+        "index": 0, #emoji在text中的位置為第0個
+        "productId": "5ac1bfd5040ab15980c9b435", #指定emoji系列
+        "emojiId": "001" #指定哪個emoji
+      },
+      {
+        "index": 13, #emoji在text中的位置為第13個
+        "productId": "5ac1bfd5040ab15980c9b435",
+        "emojiId": "002"
+      }
+    ]
+}
